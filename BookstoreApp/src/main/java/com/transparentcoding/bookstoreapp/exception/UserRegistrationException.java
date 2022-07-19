1958c6e21bac95ceb1cf51ad1e0922b6f5f952c8
nitishgupta978 <nitishgupta9785@gmail.com>
2022-07-19 12:36:03 +0530

    Added:Create an Exception class for userRegistration:

---
+++ BookstoreApp/src/main/java/com/transparentcoding/bookstoreapp/exception/UserRegistrationException.java
@@ -0,0 +1,7 @@
+package com.transparentcoding.bookstoreapp.exception;
+
+public class UserRegistrationException extends RuntimeException{
+    public UserRegistrationException (String message){
+        super(message);
+    }
+}

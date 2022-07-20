f53eb36701d7a551f23f9dff292fddf1369e3864
nitishgupta978 <nitishgupta9785@gmail.com>
2022-07-20 12:06:14 +0530

    Added: create bookStoreDto class in dto class:

---
+++ BookstoreApp/src/main/java/com/transparentcoding/bookstoreapp/dto/BookStoreDto.java
@@ -0,0 +1,26 @@
+package com.transparentcoding.bookstoreapp.dto;
+
+import lombok.ToString;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+@ToString
+public class BookStoreDto {
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "BookName  is Not valid type like ..Mahabharata")
+    public String bookName;
+
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "BookAuthor name is Not valid type like ..Guru GopalDas")
+    public String bookAuthor;
+
+    @NotEmpty(message = "Book Description not be Empty!!")
+    public String bookDescription;
+
+    public String bookLogo;
+
+    @Pattern(regexp = "^[1-9]{1}[0-9\\S]{2,}$" , message = "BookPrice not valid type like..200")
+    public String bookPrice;
+
+    @Pattern(regexp = "^[1-9]{1}[0-9\\S]{1,}$" , message = "BookQuantity not valid type like..200")
+    public String bookQuantity;
+}

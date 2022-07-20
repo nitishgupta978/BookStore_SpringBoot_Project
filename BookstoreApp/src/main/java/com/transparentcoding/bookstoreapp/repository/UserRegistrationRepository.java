cc2de1ebef3676c9b220199d320a64484484d0a6
nitishgupta978 <nitishgupta9785@gmail.com>
2022-07-20 11:09:34 +0530

    Added: add query for userLoginDto in repository class:

--- BookstoreApp/src/main/java/com/transparentcoding/bookstoreapp/repository/UserRegistrationRepository.java
+++ BookstoreApp/src/main/java/com/transparentcoding/bookstoreapp/repository/UserRegistrationRepository.java
@@ -2,8 +2,18 @@ package com.transparentcoding.bookstoreapp.repository;
 
 import com.transparentcoding.bookstoreapp.model.UserRegistration;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRegistrationRepository extends JpaRepository<UserRegistration,Long> {
+
+    @Query(value = "select * from user_registration where email_id=?1 and password = ?2", nativeQuery = true)
+    Optional<UserRegistration> findByEmail(String emailId, String password);
+
+
+    @Query(value = " select  * from user_registration here id=?1", nativeQuery = true)
+    UserRegistration getById(String id);
 }

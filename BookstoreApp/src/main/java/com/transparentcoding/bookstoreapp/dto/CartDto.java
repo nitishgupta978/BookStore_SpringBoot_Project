2885602cbb0d78a5ec4e8c2b1354db5538d5fd55
nitishgupta978 <nitishgupta9785@gmail.com>
2022-07-21 16:32:46 +0530

    Addede: create cartDto class in dtoSubpakage:

---
+++ BookstoreApp/src/main/java/com/transparentcoding/bookstoreapp/dto/CartDto.java
@@ -0,0 +1,20 @@
+package com.transparentcoding.bookstoreapp.dto;
+
+import lombok.ToString;
+import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@ToString
+@Component
+public class CartDto {
+    @Pattern(regexp = "^[1-9]{1}[0-9]$",message = "userId InValid")
+    public long userId;
+    @Pattern(regexp = "^[1-9]{1}[0-9]$",message = "bookId InValid")
+    public long bookId;
+    @NotNull(message = "Book Quantity please provide")
+    public long quantity;
+    @NotNull(message = " please provide total Price of Book ")
+    public long totalPrice;
+}

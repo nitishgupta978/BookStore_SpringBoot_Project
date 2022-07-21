b177e8da56a9b9a5fd10b0e25e011fdf932ef4be
nitishgupta978 <nitishgupta9785@gmail.com>
2022-07-21 16:21:14 +0530

    Added: create a cartDto class in model subPackage :

---
+++ BookstoreApp/src/main/java/com/transparentcoding/bookstoreapp/model/Cart.java
@@ -0,0 +1,37 @@
+package com.transparentcoding.bookstoreapp.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "cartDetails")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+     private long cartId;
+    @JsonIgnoreProperties(value = {"applications","hibernateLazyInitializer"})
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId")
+    private UserRegistration userRegistration;
+
+    @JsonIgnoreProperties(value = {"applications","hibernateLazyInitializer"})
+    @ManyToOne (fetch = FetchType.LAZY)
+    @JoinColumn(name = "bookId")
+    private BookStore bookStore;
+
+
+    private long quantity;
+    private long totalPrice;
+
+    public Cart(UserRegistration userRegistration, BookStore bookStore, long quantity, long totalPrice) {
+        this.userRegistration = userRegistration;
+        this.bookStore = bookStore;
+        this.quantity = quantity;
+        this.totalPrice = totalPrice;
+    }
+}

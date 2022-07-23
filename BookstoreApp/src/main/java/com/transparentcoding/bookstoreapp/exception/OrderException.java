1bf07db77099473b15850e4666497128ac0bf9fa
nitishgupta978 <nitishgupta9785@gmail.com>
2022-07-23 11:25:52 +0530

    Added: create orderException class for OrderDetails:

---
+++ BookstoreApp/src/main/java/com/transparentcoding/bookstoreapp/exception/OrderException.java
@@ -0,0 +1,9 @@
+package com.transparentcoding.bookstoreapp.exception;
+
+
+public class OrderException extends RuntimeException {
+
+    public OrderException(String message){
+        super(message);
+    }
+}
